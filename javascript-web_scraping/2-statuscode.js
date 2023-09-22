2c658e6d1b3a29003e8f66160ad64380b2f3d3b9
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-09-22 09:20:02 +0000

    add task 2

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+request = require('request');
+const url = process.argv[2];
+
+// Make a GET request
+request(url, (error, response) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
