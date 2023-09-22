6eafebc280ce0b53d44af8baacbf92d69a5a5750
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-09-22 09:41:53 +0000

    add task 4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } 
+    else {
+    try {
+      const filmsData = JSON.parse(body).results;
+      const antillesMovies = filmsData.filter((film) =>
+        film.characters.includes('https://swapi-api.hbtn.io/api/people/18/')
+      );
+      const count = antillesMovies.length;
+      console.log(`Number of movies with Wedge Antilles: ${count}`);
+    } catch (parseError) {
+      console.error('Error parsing API response:', parseError);
+    }
+  }
+});
