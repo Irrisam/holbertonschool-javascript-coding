8e1c62a0b59271ded78e5b2fc2050f223a263c11
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-09-22 09:26:44 +0000

    add task 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+
+request(apiUrl, (error, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    try {
+      const movieData = JSON.parse(body);
+      console.log('Title:', movieData.title);
+    } catch (parseError) {
+      console.error('Error parsing API response:', parseError);
+    }
+  }
+});
